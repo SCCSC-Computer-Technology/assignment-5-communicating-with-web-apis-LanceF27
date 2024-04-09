21e1a4e46f6ec2865c806e97b771f9afbe278ccc
LanceF27 <155991097+LanceF27@users.noreply.github.com>
2024-04-08 22:04:15 -0400

    Submitted

---
+++ ConsumeWebAPI/Controllers/StudentController.cs
@@ -0,0 +1,36 @@
+﻿using ConsumeWebAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace ConsumeWebAPI.Controllers
+{
+    public class StudentController : Controller
+    {
+        //Uri baseAddress = new Uri("https://localhost:44361/api");
+        Uri baseAddress = new Uri("https://localhost:7120/api");
+
+        private readonly HttpClient _client;
+
+        public StudentController()
+        {
+            _client = new HttpClient();
+            _client.BaseAddress = baseAddress;
+        }
+
+        [HttpGet]
+
+        public IActionResult Index()
+        {
+            List<StudentModel> studentList = new List<StudentModel>();
+            HttpResponseMessage response = _client.GetAsync(_client.BaseAddress + "/Student/Get").Result;
+
+            if(response.IsSuccessStatusCode)
+            {
+                string data = response.Content.ReadAsStringAsync().Result;
+                studentList = JsonConvert.DeserializeObject<List<StudentModel>>(data);
+            }
+
+            return View(studentList);
+        }
+    }
+}
---
+++ ConsumeWebAPI/Models/StudentModel.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ConsumeWebAPI.Models
+{
+    public class StudentModel
+    {
+        public long Id { get; set; }
+        [Required]
+        [DisplayName("Student ID")]
+        public string? Name { get; set; }
+        [Required]
+        [DisplayName("Student Name")]
+        public double GPA { get; set; }
+        [Required]
+        [DisplayName("GPA")]
+        public string? StudentEmail { get; set; }
+        [Required]
+        [DisplayName("Student Email")]
+        public string? Major { get; set; }
+        [Required]
+        [DisplayName("Major")]
+        public string? PhoneNumber { get; set; }
+        [Required]
+        [DisplayName("Phone Number")]
+        public string? StudentUserName { get; set; }
+        [Required]
+        [DisplayName("Username")]
+        public string? password { get; set; }
+        [Required]
+        [DisplayName("Student ID")]
+        public string? hashedPassword { get; set; }
+        public Boolean isVerified { get; set; }
+    }
+}
